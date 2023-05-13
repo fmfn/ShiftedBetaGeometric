09eeb4506e1d7208c1c3af844131c30f1407467e
Fernando Nogueira <fnogueira@getresq.com>
2023-05-13 18:57:00 -0400

    prep for packaging

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 from setuptools import setup, find_packages
 
 install_requires = [
-    'numpy>=1.10.0',
-    'scipy',
-    'pandas',
+    "numpy>=1.10.0",
+    "scipy>==1.7.1",
+    "pandas>=1.3.4",
 ]
 
 setup(
-    name='axia',
-    version='0.2.0',
+    name="axia",
+    version="0.1.0",
     description=(
-        'An alternative to survival analysis using ' +
-        'the shifted beta-geometric model.'
+        "An alternative to survival analysis using "
+        + "the shifted beta-geometric model."
     ),
-    classifiers=["Programming Language :: Python :: 3.5"],
-    author='Fernando Nogueira',
-    author_email='fmfnogueira@gmail.com',
-    url='',
+    classifiers=["Programming Language :: Python :: 3"],
+    author="Fernando Nogueira",
+    author_email="fmfnogueira@gmail.com",
+    url="",
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
